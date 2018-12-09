81b01809489435006c29e363b4fc107a4377437c
Corey Henderson <corman@cormander.com>
2018-12-09 10:49:41 -0700

    Initial commit of HelloVoter dev

---
+++ HelloVoter/precache-manifest.0353cee875c784605c2e459827c5a7e5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e0c0cba3b9aa500cd03",
+    "url": "/HelloVoter/static/js/runtime~main.4e0c0cba.js"
+  },
+  {
+    "revision": "272869315c743f3cdfcf",
+    "url": "/HelloVoter/static/js/main.27286931.chunk.js"
+  },
+  {
+    "revision": "e57fd78b38bcedd84edc",
+    "url": "/HelloVoter/static/js/1.e57fd78b.chunk.js"
+  },
+  {
+    "revision": "272869315c743f3cdfcf",
+    "url": "/HelloVoter/static/css/main.6a247dd0.chunk.css"
+  },
+  {
+    "revision": "ed366bd23ada154f66fdc50322d192e3",
+    "url": "/HelloVoter/index.html"
+  }
+];
