299fbb154b7f3b1c9b4aca118bc30a5e31916c6e
Corey Henderson <corman@cormander.com>
2018-12-18 13:36:48 -0700

    updates

--- HelloVoter/service-worker.js
+++ HelloVoter/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/HelloVoter/precache-manifest.e9b2a5284c8eebdd3b33b091a763b9c3.js"
+  "/HelloVoter/precache-manifest.2cbdcdccce8419d6ef378d1ee93ae74a.js"
 );
 
 workbox.clientsClaim();
