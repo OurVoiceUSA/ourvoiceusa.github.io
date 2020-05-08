26d8e9f11325612689d54ba8f3641347adb5da0a
Corey Henderson <corman@cormander.com>
2020-05-07 22:14:57 -0600

    beta for hellovoter 3.0

---
+++ hellovoter/precache-manifest.c3c94fc638548a11d3ac2e9f72c9c7a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73139fa24805fcef9fb8ea459225cf73",
+    "url": "/hellovoter/index.html"
+  },
+  {
+    "revision": "19ea05064faa1a362c4e",
+    "url": "/hellovoter/static/js/2.b410539f.chunk.js"
+  },
+  {
+    "revision": "066a3b5f5d30e9efcd56434ebe4db621",
+    "url": "/hellovoter/static/js/2.b410539f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "120e77d31e7ac88800fb",
+    "url": "/hellovoter/static/js/main.3717a99f.chunk.js"
+  },
+  {
+    "revision": "c4bf52e5f3dca18619e3",
+    "url": "/hellovoter/static/js/runtime-main.74e3fc80.js"
+  }
+]);
