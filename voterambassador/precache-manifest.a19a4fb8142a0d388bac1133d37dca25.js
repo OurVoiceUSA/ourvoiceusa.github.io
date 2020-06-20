1bbbb27bf725877237038a4f48329998b98b2c4c
Corey Henderson <corman@cormander.com>
2020-06-20 12:19:34 -0600

    staging

---
+++ voterambassador/precache-manifest.a19a4fb8142a0d388bac1133d37dca25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16d6dbc024fb2359d5b0b77afbaf38d5",
+    "url": "/voterambassador/index.html"
+  },
+  {
+    "revision": "2a5e8ecd0d9e47f7c884",
+    "url": "/voterambassador/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "31693094b8e4d70f6f48",
+    "url": "/voterambassador/static/js/2.5cd5543e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/voterambassador/static/js/2.5cd5543e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a5e8ecd0d9e47f7c884",
+    "url": "/voterambassador/static/js/main.301190ce.chunk.js"
+  },
+  {
+    "revision": "2d96281ca7eff4b345fb",
+    "url": "/voterambassador/static/js/runtime-main.58397791.js"
+  },
+  {
+    "revision": "6e1267d9d946b0236cdf6ffd02890894",
+    "url": "/voterambassador/static/media/favicon.6e1267d9.ico"
+  },
+  {
+    "revision": "fa1ded1ed7c11438a9b0385b1e112850",
+    "url": "/voterambassador/static/media/robots.fa1ded1e.txt"
+  }
+]);
